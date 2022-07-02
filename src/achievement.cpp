f6f5a194111e1a305c6762b96bca252259c766c5
casswedson <58050969+casswedson@users.noreply.github.com>
2022-07-02 16:02:46 -0500

    test: fix style

--- src/achievement.cpp
+++ src/achievement.cpp
@@ -60,7 +60,10 @@ generic_factory<achievement> achievement_factory( "achievement" );
 
 /** @relates string_id */
 template<>
-const achievement &string_id<achievement>::obj() const { return achievement_factory.obj( *this ); }
+const achievement &string_id<achievement>::obj() const
+{
+    return achievement_factory.obj( *this );
+}
 
 /** @relates string_id */
 template<>
